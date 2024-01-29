bda4bad64ef543628f61a69d130ed09b605d1691
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2024-01-29 07:25:54 +0530

    RoboShop TF Code

--- mutable.sh
+++ mutable.sh
@@ -7,6 +7,6 @@ aws ssm put-parameter --name "${ENV}.roboshop.${COMPONENT}.app_version" --value 
 
 SSH_PASSWORD=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
 # Run ansible push on the servers
-ansible-playbook -i inv roboshop.yml -e role_name=${COMPONENT} -e env=${ENV} -e version=${APP_VERSION} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}
+ansible-playbook -i inv mutable-deploy.yml -e component=${COMPONENT} -e env=${ENV} -e version=${APP_VERSION} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}
 #
 
