35eb5fde789627c5a829c6d76564547f0d307656
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2024-01-29 06:23:24 +0530

    RoboShop TF Code

---
+++ mutable.sh
@@ -0,0 +1,12 @@
+# Find the servers (Anisble Dynamic Inventory)
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-roboshop-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+
+# Change the Parameter store having the app version.
+aws ssm put-parameter --name "${ENV}.roboshop.${COMPONENT}.app_version" --value "${APP_VERSION}" --type "String" --overwrite
+
+SSH_PASSWORD=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
+# Run ansible push on the servers
+ansible-playbook -i inv roboshop.yml -e component=${COMPONENT} -e env=${ENV} -e version=${APP_VERSION} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}
+#
+
