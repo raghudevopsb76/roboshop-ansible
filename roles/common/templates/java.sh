dccf4f3d0c3a93cab32e25e9ab7febba7a9e7e84
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-02-05 07:10:07 +0530

    RoboShop TF Code

--- roles/common/templates/java.sh
+++ roles/common/templates/java.sh
@@ -1,4 +1,4 @@
 cd /app
-java -javaagent:/opt/newrelic/newrelic.jar -jar {{component}}.jar
+java -javaagent:/opt/newrelic/newrelic.jar -XX:InitialRAMPercentage=70 -XX:MaxRAMPercentage=85 -jar {{component}}.jar
 
 
